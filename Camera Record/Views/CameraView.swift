139a0edeccd02178c32cb61ce305cb5a642d3309
Marcos Luis Delgado <mluis@tuenti.com>
2025-03-19 17:11:26 +0000

    main: Added micro capabilities

--- Camera Record/Views/CameraView.swift
+++ Camera Record/Views/CameraView.swift
@@ -12,26 +12,19 @@ import Photos
 struct CameraPreview: UIViewRepresentable {
     @Binding var session: AVCaptureSession
     
-    func makeUIView(context: Context) -> VideoPreviewView {
-        let view = VideoPreviewView()
-        view.backgroundColor = .black
-        view.videoPreviewLayer.session = session
-        view.videoPreviewLayer.videoGravity = .resizeAspectFill
-        view.videoPreviewLayer.connection?.videoOrientation = .portrait
-        
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
         return view
     }
-    
-    func updateUIView(_ uiView: VideoPreviewView, context: Context) {}
-}
 
-class VideoPreviewView: UIView {
-    override class var layerClass: AnyClass {
-        AVCaptureVideoPreviewLayer.self
-    }
-    
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
-        return layer as! AVCaptureVideoPreviewLayer
+    func updateUIView(_ uiView: UIView, context: Context) {
+        if let layer = uiView.layer.sublayers?.first as? AVCaptureVideoPreviewLayer {
+          layer.session = session
+          layer.frame = uiView.bounds
+        }
     }
 }
 
@@ -140,12 +133,21 @@ class Recorder: NSObject, AVCaptureFileOutputRecordingDelegate, ObservableObject
     
     override init() {
         super.init()
+        addAudioInput()
         addVideoInput()
         if session.canAddOutput(movieOutput) {
           session.addOutput(movieOutput)
         }
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
           self?.session.startRunning()
+        }
+    }
+    
+    private func addAudioInput() {
+        guard let device = AVCaptureDevice.default(for: .audio) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: device) else { return }
+        if session.canAddInput(input) {
+          session.addInput(input)
         }
     }
     
